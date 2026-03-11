b0ba63394c46304476cbec7020f4ba3e7926189e
sirajjudin <106176479+sirajjudin@users.noreply.github.com>
2026-03-12 00:10:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     # Example of accessing an environment variable set in Azure App Service
     website_name = os.environ.get("WEBSITE_SITE_NAME", "World")
-    return f"<h1>Hello, {website_name}!</h1>"
+    return f"<h1>Hello,Guys!</h1>"
 
 if __name__ == "__main__":
     # The port can be dynamically set by Azure, so we use an environment variable
